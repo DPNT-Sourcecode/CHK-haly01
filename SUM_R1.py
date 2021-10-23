f104ac6f562ce9073508ecb095b9b895d45a8643
sbx_user1051 <sbx_user1051@169.254.215.205>
2021-10-23 13:56:07 +0000

    Sat Oct 23 13:56:07 UTC 2021

--- SUM_R1.py
+++ SUM_R1.py
@@ -1,16 +1,16 @@
 
 def addInt(param):
     try:
-        val = param[0]+param[1]
-        return val
+        if 0<=param[0]<=100 and 0<=param[0]<=100:
+            val = param[0]+param[1]
+            return val
+        else:
+            return 'Please enter positive integers between 0-100'
     except:
         return f'Invalid parameters {param[0]} and {param[1]}'
 
-
-
 def main():
-    print(addInt([21, 99]))
-    
+    print(addInt([210, 99]))
     
 
 if __name__ == "__main__":
