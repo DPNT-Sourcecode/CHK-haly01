3a89ba62fbe4e80d7751ce017eef89edc8416e5c
sbx_user1051 <sbx_user1051@169.254.215.205>
2021-10-23 14:13:10 +0000

    Sat Oct 23 14:13:10 UTC 2021

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,7 +3,10 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    return f'Hello, World!'
+    try:
+        return f'Hello, {str(friend_name)}!'
+    except TypeError:
+        return f'Hello, {friend_name}!'
 
 
 
