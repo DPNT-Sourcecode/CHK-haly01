7bf7c68ed936fee8287b5fc4074a875ee52d29bb
sbx_user1051 <sbx_user1051@169.254.215.205>
2021-10-23 14:03:34 +0000

    Sat Oct 23 14:03:34 UTC 2021

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,11 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def compute(int1, int2):
+    try:
+        if 0<=int1<=100 and 0<=int2<=100:
+            val = int1+int2
+            return val
+        else:
+            return f'Please enter positive integers between 0-100 instead of {int1} and {int2}'
+    except:
+        return f'Invalid parameters {int1} and {int2}'
+
