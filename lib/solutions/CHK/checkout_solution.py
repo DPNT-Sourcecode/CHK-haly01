e3e50608d967a023b77b114335578fc62b181457
sbx_user1051 <sbx_user1051@169.254.215.205>
2021-10-23 14:23:04 +0000

    Sat Oct 23 14:23:04 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,21 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+class items:
+    def __init__(self, Item, Price):
+        self.Item = Item
+        self.Price = Price
+
+
+class offItems(items):
+    def __init__(self, Item, Price, sOffNum, sOffValue):
+        super.__init__(Item, Price)
+        self.sOffNum = sOffNum
+        self.sOffValue = sOffValue
+
+        
+        
 def checkout(skus):
     raise NotImplementedError()
+
