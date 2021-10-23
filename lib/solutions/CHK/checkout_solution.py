6e58b00462c10504a085a5a8ca44e159288c0293
sbx_user1051 <sbx_user1051@169.254.215.205>
2021-10-23 16:37:53 +0000

    Sat Oct 23 16:37:53 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -42,10 +42,8 @@ class sItems(items):
 
     def tPrice(self, iNumber, fBasket):
         sPrice = iNumber*self.Price
-        print(self.freeItem.sOffValue * int(int(iNumber/self.sOffNum)/self.freeItem.sOffNum))
-        print((int(iNumber/self.sOffNum)%self.freeItem.sOffNum)*(self.freeItem.Price*self.freeItem.sOffNum - self.freeItem.sOffValue))
         discount = (self.freeItem.sOffValue * int(int(iNumber/self.sOffNum)/self.freeItem.sOffNum) + 
-        (int(iNumber/self.sOffNum)%self.freeItem.sOffNum)*(self.freeItem.Price*self.freeItem.sOffNum - self.freeItem.sOffValue)
+        (int(iNumber/self.sOffNum)%self.freeItem.sOffNum)*((self.freeItem.Price*self.freeItem.sOffNum-self.freeItem.sOffValue) if fBasket.count(self.freeItem.Item)%self.freeItem.sOffNum == 0 else self.freeItem.Price)
         if int(iNumber/self.sOffNum)*self.freeItemNum < fBasket.count(self.freeItem.Item)
         else self.freeItem.sOffValue*int(fBasket.count(self.freeItem.Item)/self.freeItem.sOffNum) + 
         (fBasket.count(self.freeItem.Item)%self.freeItem.sOffNum)*self.freeItem.Price)
@@ -69,5 +67,3 @@ def checkout(skus):
         return sum(basPrices)
     except:
         return -1
-
-print(checkout('AAAAAEEBAAABB'))
