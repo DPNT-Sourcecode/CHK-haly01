f5a5f9fa62e8cdff8799232523cd47c832527262
sbx_user1051 <sbx_user1051@169.254.215.205>
2021-10-23 13:53:07 +0000

    Sat Oct 23 13:53:07 UTC 2021

--- SUM_R1.py
+++ SUM_R1.py
@@ -1,7 +1,17 @@
 
+def addInt(param):
+    try:
+        val = param[0]+param[1]
+        return val
+    except:
+        return f'Invalid parameters {param[0]} and {param[1]}'
+
+
 
 def main():
-    pass
+    print(addInt([21, 99]))
+    
+    
 
 if __name__ == "__main__":
     main()
