168671d3ac523fb09fb9770a19254df8ed8e1a05
sbx_user1051 <sbx_user1051@169.254.215.205>
2021-10-23 15:29:06 +0000

    Sat Oct 23 15:29:06 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,7 +8,7 @@ class items:
         self.Item = Item
         self.Price = Price
     
-    def tPrice(self, iNumber):
+    def tPrice(self, iNumber, _):
         return self.Price * iNumber
 
 
@@ -18,7 +18,7 @@ class offItems(items):
         self.sOffNum = sOffNum
         self.sOffValue = sOffValue
 
-    def tPrice(self, iNumber):
+    def tPrice(self, iNumber, _):
         return self.sOffValue*int(iNumber/self.sOffNum) + (iNumber%self.sOffNum)*self.Price
 
 
@@ -29,20 +29,28 @@ class sItems(items):
         self.freeItem = freeItem
         self.freeItemNum = freeItemNum
 
-    def tPrice(self, iNumber):
-        return self.sOffValue*int(iNumber/self.sOffNum) + (iNumber%self.sOffNum)*self.Price
+    def tPrice(self, iNumber, fBasket):
+        sPrice = self.sOffValue*int(iNumber/self.sOffNum) + (iNumber%self.sOffNum)*self.Price
+        discount = int(iNumber/self.sOffNum)*self.freeItem.Price*self.freeItemNum if iNumber*self.freeItemNum <= fBasket.count(self.freeItem.Name) else fBasket.count(self.freeItem.Name)*self.freeItem.Price
+        return sPrice-discount
 
 def checkout(skus):
-    itList = [offItems('A', 50, 3, 130), offItems('B', 30, 2, 45), items('C', 20), items('D', 15)]
+    itA = offItems('A', 50, 3, 130)
+    itB = offItems('B', 30, 2, 45)
+    itC = items('C', 20)
+    itD = items('D', 15)
+    itE = sItems('E', 40, 2, itB, 1)
+    
+    itList = [itA, itB, itC, itD, itE]
     try:
         basket=[vals for vals in skus]
         uItems = set(basket)
         basPrices=[]
         for i in uItems:
-            basPrices.append([x.tPrice(basket.count(i)) for x in itList if x.Item == i][0])
-        
+            basPrices.append([x.tPrice(basket.count(i), basket) for x in itList if x.Item == i][0])
         return sum(basPrices)
-    except:
-        return -1
+    except Exception as e:
+        return e
 
+print(checkout('AAABBED'))
 
