2c7c2ad2088040ee82d5ea23dcdbcc0cdb3492d1
sbx_user1051 <sbx_user1051@169.254.215.205>
2021-10-23 16:08:06 +0000

    Sat Oct 23 16:08:06 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -32,8 +32,8 @@ class sItems(items):
     def tPrice(self, iNumber, fBasket):
         sPrice = iNumber*self.Price
         discount = (self.freeItem.sOffValue*int(int(iNumber/self.sOffNum)/self.freeItem.sOffNum) + 
-        (int(iNumber/self.sOffNum)%self.freeItem.sOffNum)*self.freeItem.Price           
-        if iNumber*self.freeItemNum <= fBasket.count(self.freeItem.Item)
+        (int(iNumber/self.sOffNum)%self.freeItem.sOffNum)*self.freeItem.Price         
+        if int(iNumber/self.sOffNum)*self.freeItemNum <= fBasket.count(self.freeItem.Item)
         else self.freeItem.sOffValue*int(fBasket.count(self.freeItem.Item)/self.freeItem.sOffNum) + 
         (fBasket.count(self.freeItem.Item)%self.freeItem.sOffNum)*self.freeItem.Price)
 
@@ -58,5 +58,4 @@ def checkout(skus):
     except Exception as e:
         return e
 
-print(checkout('AAABBEEEEEED'))
-
+print(checkout('AAABBEED'))
